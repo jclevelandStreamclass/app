7e7a73577da62c1bdf9526e88875a2b15a8f4b24
Rubenrh <ruben.romeroherrero@gmail.com>
2021-07-12 16:31:09 +0200

    architecture frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+import { LandingComponent } from './core/views/landing/landing.component';
+import { HomeComponent } from './core/views/home/home.component';
+import { LoginComponent } from './core/views/login/login.component';
+import { SignupComponent } from './core/views/signup/signup.component';
+import { RecoverpasswordComponent } from './core/views/recoverpassword/recoverpassword.component';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, LandingComponent, HomeComponent, LoginComponent, SignupComponent, RecoverpasswordComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
