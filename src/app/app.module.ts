2366c5ff5479cfead226f8a53d79f5f7b96bf6a4
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-13 17:43:55 +0200

    landind html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,32 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
-import { HeaderComponent } from './core/components/header/header.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
 import { FooterComponent } from './core/components/footer/footer.component';
-import { LandingComponent } from './core/views/landing/landing.component';
-import { HomeComponent } from './core/views/home/home.component';
-import { LoginComponent } from './core/views/login/login.component';
-import { SignupComponent } from './core/views/signup/signup.component';
-import { RecoverpasswordComponent } from './core/views/recoverpassword/recoverpassword.component';
+import { HeaderComponent } from './core/components/header/header.component';
 import { AppComponent } from './core/views/app.component';
+import { HomeComponent } from './core/views/home/home.component';
+import { LandingComponent } from './core/views/landing/landing.component';
+import { LoginComponent } from './core/views/login/login.component';
+import { RecoverpasswordComponent } from './core/views/recoverpassword/recoverpassword.component';
+import { SignupComponent } from './core/views/signup/signup.component';
+
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, LandingComponent, HomeComponent, LoginComponent, SignupComponent, RecoverpasswordComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent,
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    RecoverpasswordComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -23,6 +34,8 @@ import { AppComponent } from './core/views/app.component';
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
+    MatInputModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
