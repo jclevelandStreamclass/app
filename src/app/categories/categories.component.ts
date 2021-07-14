2363762730888739acdab01ba0a8c22ae3832a70
PAbro <pabmengibar@gmail.com>
2021-07-14 15:51:16 +0200

    categorias en landing

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from './interface/category.interface';
+import { CategoriesService } from './services/categories.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,11 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
-  constructor( ) { }
+  categories!: Category[];
+  constructor(private categorySvc: CategoriesService) { }
 
   ngOnInit(): void {
-
-  }
-  
-
+    this.categorySvc.getCategory().subscribe(x => {this.categories = x});
+    console.log(this.categories)
+}
 }
--- src/app/core/views/landing/landing.component.ts
+++ src/app/core/views/landing/landing.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Category } from 'src/app/categories/interface/category.interface';
+import { CategoriesService } from 'src/app/categories/services/categories.service';
 
 @Component({
   selector: 'app-landing',
@@ -8,17 +10,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class LandingComponent implements OnInit {
   emailForm: FormGroup;
+  categories!: Category[];
 
   @Output() sendEmail = new EventEmitter<string>();
 
-  constructor(fb: FormBuilder) {
+  constructor(fb: FormBuilder, private categorySvc: CategoriesService) {
     this.emailForm = fb.group({
       email: [''],
     });
   }
   
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.categorySvc.getCategory().subscribe(x => {this.categories = x});
+    console.log(this.categories)
+  }
 
   registerEmail(form: FormGroup): void {
     if (form.valid) {
