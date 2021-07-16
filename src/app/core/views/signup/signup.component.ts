648874bdfb103dd8c8ef0f72e7f64de4fcea7c93
Miguel <miguelbermell@gmail.com>
2021-07-16 16:33:47 +0200

    sign up

--- src/app/core/views/signup/signup.component.ts
+++ src/app/core/views/signup/signup.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SignupModelService } from './services/signup-model.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  signUpForm: FormGroup;
+  error = false;
+  constructor(fb: FormBuilder, private signUpModel: SignupModelService) {
+    this.signUpForm = fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      name: ['', Validators.required],
+      phone: ['', Validators.required],
+      avatar: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  saveClick(form: FormGroup) {
+    if (form.valid) {
+      console.log(form.value);
+      return;
+    }
+
+    this.error = true;
+  }
 }
