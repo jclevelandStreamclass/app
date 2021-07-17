bdd4eb54b9ebf85ed61d0b01eb9a4a2b00e7012c
Miguel <miguelbermell@gmail.com>
2021-07-17 18:52:33 +0200

    sign Up

--- src/app/core/views/signup/signup.component.ts
+++ src/app/core/views/signup/signup.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { UserModel } from 'src/app/models/user';
 import { SignupModelService } from './services/signup-model.service';
 
 @Component({
@@ -12,6 +19,8 @@ export class SignupComponent implements OnInit {
   signUpForm: FormGroup;
   error = false;
   userEmail: string = '';
+  avatar!: string;
+  @ViewChild('avatarElement') avatarElement!: ElementRef<HTMLParagraphElement>;
   constructor(
     fb: FormBuilder,
     private signUpModel: SignupModelService,
@@ -30,15 +39,32 @@ export class SignupComponent implements OnInit {
     });
   }
 
+  // ngAfterViewInit(): void {
+  //   this.signUpForm
+  //     .get('avatar')
+  //     ?.valueChanges.subscribe((data) => (this.avatar = data.slice(12)));
+  // }
   ngOnInit(): void {
     if (this.userEmail) {
       this.signUpForm.controls.email.setValue(this.userEmail);
     }
+    this.avatar = this.signUpForm.controls.avatar.value;
+    console.log(this.avatar);
+  }
+
+  onFilePic(e: Event): void {
+    this.avatar = (e.target as HTMLInputElement).files![0].name;
   }
 
   saveClick(form: FormGroup) {
     if (form.valid) {
-      console.log(form.value);
+      const user: UserModel = form.value;
+      user.avatar = user.avatar.slice(12);
+      this.signUpModel.insertUser(user).subscribe(() => {
+        console.log(user);
+        this.signUpForm.reset();
+        this.avatar = '';
+      });
       return;
     }
 
