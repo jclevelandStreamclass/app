c39f65eac379b6e779b0d804d6a5144adeda5a5b
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-18 19:59:15 +0200

    implement serie-grid selector click

--- src/app/series/series-grid-landing/series-grid-landing.component.ts
+++ src/app/series/series-grid-landing/series-grid-landing.component.ts
@@ -1,18 +1,22 @@
 import { Input, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Serie } from '../models/serie';
 
 @Component({
   selector: 'app-series-grid-landing',
   templateUrl: './series-grid-landing.component.html',
-  styleUrls: ['./series-grid-landing.component.scss']
+  styleUrls: ['./series-grid-landing.component.scss'],
 })
 export class SeriesGridLandingComponent implements OnInit {
-
   @Input() series: Serie[] = [];
-  
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {}
+
+  select(serieId: string): void {
+    if (serieId) {
+      this.router.navigate(['series', serieId]);
+    }
   }
-
 }
