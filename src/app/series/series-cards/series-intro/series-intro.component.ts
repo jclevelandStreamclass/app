73faa1ac12f45ffc663b619eaea7a296ddd131e0
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-18 20:00:50 +0200

    series intro dyanmic html. getter spsplayer serv

--- src/app/series/series-cards/series-intro/series-intro.component.ts
+++ src/app/series/series-cards/series-intro/series-intro.component.ts
@@ -1,28 +1,46 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Episode } from 'src/app/episodes/models/episode';
 import { SportsPlayer } from 'src/app/sportsplayers/models/sportsPlayer';
 import { Serie } from '../../models/serie';
-
-
+import { SeriesService } from '../../services/series.service';
 
 @Component({
   selector: 'app-series-intro',
   templateUrl: './series-intro.component.html',
-  styleUrls: ['./series-intro.component.scss']
+  styleUrls: ['./series-intro.component.scss'],
 })
 export class SeriesIntroComponent implements OnInit {
+  serieId: string = '';
+  series!: Serie;
+  episodes: Episode[] = [];
+  sportsPlayer!: SportsPlayer | null;
+  //TODO check !
 
-  @Input() sportsPlayer: SportsPlayer[] | null = [];
-  @Input() series: Serie[] | null = [];
+  constructor(
+    private route: ActivatedRoute,
+    private seriesModel: SeriesService
+  ) {
+    route.params.subscribe((params) => {
+      this.serieId = params.serieId || '';
+    });
+  }
 
-  constructor() {
-   
-  }
-  
-  ngOnChanges(): void {
-    
-  }
+  ngOnChanges(): void {}
 
   ngOnInit(): void {
+    if (this.serieId) {
+      this.seriesModel.getSerieById(this.serieId).subscribe((s) => {
+        this.series = s;
+        this.episodes = [...s.episodes];
+        console.log(this.episodes);
+        this.seriesModel
+          .getSportsPlayerById(s.sportsPlayerId)
+          .subscribe((sp) => {
+            this.sportsPlayer = sp;
+            console.log(this.sportsPlayer);
+          });
+      });
+    }
   }
-
 }
