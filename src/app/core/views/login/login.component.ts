abf976c08c4b021f670c38fbb0dad72b825897d7
Miguel <miguelbermell@gmail.com>
2021-07-19 18:23:29 +0200

    login toastService

--- src/app/core/views/login/login.component.ts
+++ src/app/core/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
+import { ToastMessagesService } from '../../services/toast-messages.service';
 
 import { LoginModelService } from './services/login-model.service';
 
@@ -17,7 +18,8 @@ export class LoginComponent implements OnInit {
     fb: FormBuilder,
     private loginModel: LoginModelService,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private toastService: ToastMessagesService
   ) {
     this.loginForm = fb.group({
       email: ['', Validators.required],
@@ -34,9 +36,13 @@ export class LoginComponent implements OnInit {
   saveClick(form: FormGroup): void {
     if (form.valid) {
       this.loginModel.login(form.value).subscribe((user) => {
-        if (user) {
+        if (user.active) {
           this.authService.storeUser(user);
           this.router.navigate(['/landing']);
+        } else {
+          this.toastService.showError(
+            'Tienes que activar tu cuenta, revisa tu correo electrónico'
+          );
         }
       });
       return;
