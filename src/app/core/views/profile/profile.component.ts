051eee33f3301c56a748832dd6eb46b46bd7a742
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-20 17:21:35 +0200

    profile user data responsive

--- src/app/core/views/profile/profile.component.ts
+++ src/app/core/views/profile/profile.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/models/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  user!: UserModel | null;
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.user = this.authService.user;
   }
-
+  signout(): void {
+    this.authService.logOutUser();
+  }
 }
