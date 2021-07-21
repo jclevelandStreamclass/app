192c0fbbbdf1bdc8b8449eed1df1951f615f00f6
Rubenrh <ruben.romeroherrero@gmail.com>
2021-07-21 23:39:02 +0200

    random implement by Miguel

--- src/app/series/series-grid-landing/series-grid-landing.component.ts
+++ src/app/series/series-grid-landing/series-grid-landing.component.ts
@@ -1,4 +1,11 @@
-import { Input, Component, OnInit } from '@angular/core';
+import {
+  Input,
+  Component,
+  OnInit,
+  AfterViewInit,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { Serie } from '../models/serie';
 
@@ -7,11 +14,19 @@ import { Serie } from '../models/serie';
   templateUrl: './series-grid-landing.component.html',
   styleUrls: ['./series-grid-landing.component.scss'],
 })
-export class SeriesGridLandingComponent implements OnInit {
+export class SeriesGridLandingComponent implements OnInit, OnChanges {
   @Input() series: Serie[] = [];
+  // seriesPintar: Serie[] = [];
+  // randomNumber!: number;
 
   constructor(private router: Router) {}
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  random = () => Math.random() - 0.5;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.series) {
+      //console.log(this.series.sort(this.random));
+    }
   }
 
   select(serieId: string): void {
