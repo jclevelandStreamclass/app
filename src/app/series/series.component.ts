20c9a4755e22f9424bc9f1e92b8de8c8004dedd1
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-22 08:07:42 +0200

    added '/home' checkroute

--- src/app/series/series.component.ts
+++ src/app/series/series.component.ts
@@ -1,27 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Serie } from './models/serie'
+import { Serie } from './models/serie';
 import { SeriesService } from './services/series.service';
 @Component({
   selector: 'app-series',
   templateUrl: './series.component.html',
-  styleUrls: ['./series.component.scss']
+  styleUrls: ['./series.component.scss'],
 })
 export class SeriesComponent implements OnInit {
-
   series: Serie[] = [];
 
-  constructor(private serviceModel: SeriesService, private router: Router) { }
+  constructor(private serviceModel: SeriesService, private router: Router) {}
 
   // Lanzamos las series de la DB al iniciar la ruta
   ngOnInit(): void {
-    this.serviceModel.getSeries().subscribe(result => {
+    this.serviceModel.getSeries().subscribe((result) => {
       this.series = result;
-    })
+    });
   }
 
   checkRoute(): boolean {
-    return this.router.url === '/landing';
+    return this.router.url === '/landing' || this.router.url === '/home';
   }
-
 }
