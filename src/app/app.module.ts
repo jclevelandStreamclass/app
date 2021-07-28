4af02fb58a479928d926595e91c9265b243afa75
Madeleine Elliot <81318490+mad-prog@users.noreply.github.com>
2021-07-28 21:58:28 +0200

    removed unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app-routing.module';
-import { CoreModule } from './core/core.module';
-import { AppComponent } from './core/views/app.component';
 import { HotToastModule } from '@ngneat/hot-toast';
-import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 import { ContactsComponent } from './contacts/contacts.component';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CoreModule } from './core/core.module';
 import { AuthInterceptor } from './core/services/auth.interceptor';
-import { AdminComponent } from './admin/admin.component';
-import { CategoriesListComponent } from './admin/categories-list/categories-list.component';
-import { SportsPlayerListComponent } from './admin/sports-player-list/sports-player-list.component';
-import { MatTableModule } from '@angular/material/table';
+import { AppComponent } from './core/views/app.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
